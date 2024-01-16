7c2807bfef8599bffd4d617e651eea6b4992d3a7
taijulsir <princetaijul123@gmail.com>
2024-01-16 23:53:38 +0600

    added navbar

--- src/app/page.js
+++ src/app/page.js
@@ -2,8 +2,8 @@ import ingredients from '../../ingredients.json'
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1 className="text-4xl font-bold text-center">Recipe app</h1>
-    </main>
+    <div>
+
+    </div>
   )
 }
